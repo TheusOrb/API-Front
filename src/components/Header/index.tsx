f8fbcc31fc8fa27d5142b6bacc0528f58be27337
Matheus Orbach <matheus.orbach@gmail.com>
2022-10-07 21:47:21 -0300

    Rotas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
-import { HomeHeader, Menu } from '../../components/Header/styles';
-import { Button } from '../Button/styles';
-
-
+import Link from "next/link";
+import { HomeHeader, Menu } from "../../components/Header/styles";
+import { Button } from "../Button/styles";
 
 const Header: React.FC = () => {
   return (
@@ -11,10 +10,18 @@ const Header: React.FC = () => {
         <h1>Aletur</h1>
       </HomeHeader>
       <Menu>
-        <Button>Home</Button>
-        <Button>Frota</Button>
-        <Button>Manutenção</Button>
-        <Button>Alertas</Button>
+        <Link href={"/home"}>
+          <Button>Home</Button>
+        </Link>
+        <Link href={"/frota"}>
+          <Button>Frota</Button>
+        </Link>
+        <Link href={"/manutencao"}>
+          <Button>Manutenção</Button>
+        </Link>
+        <Link href={"/alertas"}>
+          <Button>Alertas</Button>
+        </Link>
       </Menu>
     </>
   );
